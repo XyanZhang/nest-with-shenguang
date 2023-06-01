b9cadc604f9ea821b458daf83d9e5df92e8d1333
XyanZhang <1743067175@qq.com>
2023-06-01 22:01:48 +0800

    feat: usefilter

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Get, Render, Inject, HttpException, HttpStatus, UseFilters } from '@nestjs/common';
+import { Controller, Get, Render, Inject, HttpException, HttpStatus, UseFilters, Session, Ip, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ErrFilter } from './err.filter';
+import { AppGuard } from './app.guard';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   // @Inject('Kenan')
   // public readonly kenan: string;
-
+  @UseGuards(AppGuard)
   @UseFilters(ErrFilter)
   @Get()
   getHello(): any {
@@ -25,5 +26,20 @@ export class AppController {
   @Get("/error")
   getError(): any {
     throw new HttpException('倒霉蛋', HttpStatus.BAD_REQUEST);
+  }
+
+  @Get('/session')
+  session(@Session() session) {
+    if(!session.count) {
+      session.count = 0;
+    }
+    session.count = session.count + 1;
+    return session.count;
+  }
+
+  @Get('/ip')
+  ip(@Ip() ip: string) {
+    console.log(ip);
+    return ip
   }
 }
